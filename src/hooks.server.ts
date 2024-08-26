3f95f4a207cde7ddc735506554a97c759f7ab08e
Idan Gazit <idan@gazit.me>
2024-08-26 16:29:33 -0700

    Add pathname header

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import type { Handle } from "@sveltejs/kit";
 export const handle: Handle = async ({ event, resolve }) => {
 	event.setHeaders({ 'Arr-Disable-Session-Affinity': 'true' });
 	event.setHeaders({ 'X-My-Hostname-Is': event.url.hostname });
+	event.setHeaders({ 'X-My-Path-Is': event.url.pathname });
 	event.cookies.set('testcookie', 'testcookie', {
 		path: '/',
 		maxAge: 31536000000,
