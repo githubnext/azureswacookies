c0c883e9e532ee1ca2143a24e47424bd3fca3eb7
Idan Gazit <idan@gazit.me>
2024-08-26 16:20:31 -0700

    add hostname to response header

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,11 +2,13 @@ import type { Handle } from "@sveltejs/kit";
 
 export const handle: Handle = async ({ event, resolve }) => {
 	event.setHeaders({ 'Arr-Disable-Session-Affinity': 'true' });
+	event.setHeaders({ 'X-My-Hostname-Is': event.url.hostname });
 	event.cookies.set('testcookie', 'testcookie', {
 		path: '/',
 		maxAge: 31536000000,
 		sameSite: "lax",
 		httpOnly: false,
+		domain: event.url.hostname,
 	});
 	return await resolve(event);
 };
