edba95cdeb0f02661053606a03015099b4aa47e3
Idan Gazit <idan@gazit.me>
2024-08-27 14:47:27 -0700

    Also set a >1y max-age on page cookie

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({cookies}) => {
     console.log('SvelteKit load function processed a request.');
-    cookies.set('pagecookiename', 'pagecookievalue', {'path': '/'});
+    cookies.set('pagecookiename', 'pagecookievalue', {'path': '/', maxAge: 31536001});
 }
