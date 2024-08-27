d8bef198dba1f9ef7692f2dd44e5d81c2f6375cb
Idan Gazit <idan@gazit.me>
2024-08-27 15:28:26 -0700

    Even more max-age explorations

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,5 +2,5 @@ import type { PageServerLoad } from './$types';
 
 export const load: PageServerLoad = async ({cookies}) => {
     console.log('SvelteKit load function processed a request.');
-    cookies.set('pagecookiename', 'pagecookievalue', {'path': '/', maxAge: 315360000});
+    cookies.set('pagecookiename', 'pagecookievalue', {'path': '/', maxAge: 3153699999});
 }
