41656538614f5ca1ab8f228e8be05093d20903b7
Idan Gazit <idan@gazit.me>
2024-08-26 17:17:44 -0700

    Add a +page.server.ts cookie

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,6 @@
+import type { PageServerLoad } from './$types';
+
+export const load: PageServerLoad = async ({cookies}) => {
+    console.log('SvelteKit load function processed a request.');
+    cookies.set('pagecookiename', 'pagecookievalue', {'path': '/'});
+}
